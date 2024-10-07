d03fed7b5b6dabbb46381b58e5d3c9c667b07452
koreaflash <koreaflash@naver.com>
2024-10-07 18:16:44 +0900

    scg, eureka

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api"; // 백엔드 서버 주소에 맞게 수정
+const BASE_URL = "http://localhost:19080/api/subrecommend"; // 백엔드 서버 주소에 맞게 수정
 
 export const fetchRecommendedCategory = async (userId) => {
   const response = await axios.get(
